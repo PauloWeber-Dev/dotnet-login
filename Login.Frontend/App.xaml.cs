8f759157f6154f772c6829b3d765183cf6c9abcc
Paulo Weber <pauloweber.dev@gmail.com>
2025-06-29 21:12:38 -0300

    Commit inicial - Solução e Projetos

---
+++ Login.Frontend/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Login.Frontend
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
